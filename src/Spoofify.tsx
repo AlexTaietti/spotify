e99c4151f56b2d09e43402fee2a4ece39130e62f
AlexTaietti <taiettialex@yahoo.it>
2021-03-11 19:37:43 +0000

    🚀 IMPROVEMENT: change the App file's name

---
+++ src/Spoofify.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavBar, TrackContainer } from './components';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const AppWrapper = styled.div`
+
+  display: grid;
+  grid-template-areas:
+  "nav-bar main"
+  "track track";
+  grid-template-columns: 250px auto;
+  grid-template-rows: auto 100px;
+  position: relative;
+  height: 100%;
+  font-family: sans-serif;
+
+`;
+
+export const Spoofify: React.FC = () => {
+  return (
+    <AppWrapper>
+      <Router>
+        <NavBar />
+        <TrackContainer />
+      </Router>
+    </AppWrapper>
+  );
+}
+
+export default Spoofify;
