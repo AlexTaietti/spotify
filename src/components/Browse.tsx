4e1ea24d866a3634fc8956c92c77a17ebe8b291d
AlexTaietti <taiettialex@yahoo.it>
2021-03-14 00:43:48 +0000

    🚀 IMPROVEMENT: add base templates for the main content sections

---
+++ src/components/Browse.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Browse: React.FC = () => {
+
+   return null;
+
+};
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Carousel } from './Carousel';
+
+export const Home: React.FC = () => {
+
+   return <Carousel />;
+
+};
