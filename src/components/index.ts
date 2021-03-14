e75df54e0bc23daba6a187f66d55de655ae1c6e5
AlexTaietti <taiettialex@yahoo.it>
2021-03-14 00:49:33 +0000

    📝 CHORE: update exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 export { NavBar } from './NavBar';
 export { TrackContainer } from './TrackContainer';
 export { MainContainer } from './MainContainer';
+export { PlaylistCard } from './PlaylistCard';
+export { Home } from './Home';
+export { Browse } from './Browse';
+export { LikedSongs } from './LikedSongs';
+export { Carousel } from './Carousel';
