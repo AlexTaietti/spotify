07ce767bd29461fb824a4ad3aa947515c2af3998
AlexTaietti <taiettialex@yahoo.it>
2021-03-16 19:18:49 +0000

    🔧 FIX: rremove unused exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,3 @@
 export { NavBar } from './NavBar';
 export { TrackContainer } from './TrackContainer';
 export { MainContainer } from './MainContainer';
-export { PlaylistCard } from './PlaylistCard';
-export { Home } from './Home';
-export { Browse } from './Browse';
-export { LikedSongs } from './LikedSongs';
-export { Carousel } from './Carousel';
