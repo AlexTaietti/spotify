964f63e4fa1785114784ada09639ceca6c1f9159
AlexTaietti <taiettialex@yahoo.it>
2021-03-17 13:35:05 +0000

    🚀 IMPROVEMENT: add playlist view component to be displayed when a playlist card is clicked

---
+++ src/components/PlaylistView.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react';
+//import { useLocation } from 'react-router';
+//import { useSpotifyApi } from '../hooks/useSpotifyApi';
+
+//type PlaylistLocationState = { id: string; }
+
+export const PlaylistView: React.FC = () => {
+
+   //const { state: locationState } = useLocation<PlaylistLocationState>();
+
+   //const playlistInfo = useSpotifyApi(`/get_playlist_tracks/${locationState.id}`);
+
+   return <h1>Helloooooo!</h1>;
+
+};
