d3f3e0364b9286e2498d092bdbbf3e57509088c1
AlexTaietti <taiettialex@yahoo.it>
2021-03-21 21:44:52 +0000

    🚀 IMPROVEMENT: create the first piece of the playback bar

--- src/components/TrackAlbumData.tsx
+++ src/components/TrackAlbumData.tsx
@@ -1,13 +1,56 @@
 import styled from 'styled-components';
 
 type AlbumDataProps = {
-   albumImage: string;
-   songTitle: string;
-   album: string;
+   image: string;
+   song: string;
+   artist: string;
 }
 
-export const AlbumData: React.FC<AlbumDataProps> = () => {
+const AlbumDataContainer = styled.div`
 
-   return null;
+   height: 100%;
+   position: relative;
+   display: flex;
+
+   img{
+      display: inline-block;
+      height: 100%;
+      width: 100px;
+   }
+
+   div{
+
+      display: inline-flex;
+      justify-content: center;
+      flex-direction: column;
+      height: 100%;
+      padding: 20px 30px;
+      
+      span:first-of-type{
+         font-weight: 400;
+         font-size: 2.4rem;
+         margin-bottom: 8px;
+      }
+
+      span:last-of-type{
+         font-weight: 300;
+         font-size: 1.8rem;
+      }
+
+   }
+
+`;
+
+export const AlbumData: React.FC<AlbumDataProps> = ({ image, song, artist }) => {
+
+   return (
+      <AlbumDataContainer>
+         <img alt='playlist thumbnail' src={image} />
+         <div>
+            <span>{song}</span>
+            <span>{artist}</span>
+         </div>
+      </AlbumDataContainer>
+   );
 
 };
--- src/components/TrackContainer.tsx
+++ src/components/TrackContainer.tsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import { AlbumData } from './TrackAlbumData';
+
+const TrackWrapper = styled.div`
+
+   grid-area: track;
+   position: relative;
+   display: flex;
+   justify-content: space-between;
+   
+`;
+
 export const TrackContainer: React.FC = () => {
 
-   return null;
+   const mockSongData = {
+      image: 'http://api.sprintt.co/spotify/images/pop/p_9.jpg',
+      songName: 'Square One',
+      artist: 'Coldplay'
+   };
+
+   return (
+      <TrackWrapper>
+         <AlbumData song={mockSongData.songName} artist={mockSongData.artist} image={mockSongData.image} />
+      </TrackWrapper>
+   );
 
 };
