7b6dbe359fc1c0d2d329f6944f6eb95769aed6f5
AlexTaietti <taiettialex@yahoo.it>
2021-03-30 00:04:16 +0100

    🚀 IMPROVEMENT: fetch and display the list of liked songs

--- src/components/LikedSongs.tsx
+++ src/components/LikedSongs.tsx
@@ -1,1 +1,9 @@
-export const LikedSongs: React.FC = () => { return null; };
+import { useSpotifyApi } from '../state/hooks/useSpotifyApi';
+
+export const LikedSongs: React.FC = () => {
+
+   const { response: playlistInfo } = useSpotifyApi<any>(`liked_tracks`);
+
+   return <pre style={{ padding: '100px' }}>{JSON.stringify(playlistInfo, null, 3)}</pre>;
+
+};
